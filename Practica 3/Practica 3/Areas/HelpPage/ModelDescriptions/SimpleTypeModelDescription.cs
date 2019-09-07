fd719e8e3897630cf310639610dd3163c2e76796
sergio hoyos <yeyo_hoyos@hotmail.com>
2019-09-06 21:06:46 -0400

    primer api

---
+++ Practica 3/Practica 3/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Practica_3.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Practica 3/Practica 3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica_3.WebApiApplication" Language="C#" %>
